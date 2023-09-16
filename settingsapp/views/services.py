6a0a371595e6a75ccf7e40431b6cecf56b05ff54
Fernando Cuevas Jr <41988018+clsvar@users.noreply.github.com>
2023-09-16 15:29:19 +0800

    Update services.py
    change hash

--- settingsapp/views/services.py
+++ settingsapp/views/services.py
@@ -5,6 +5,6 @@ import hashlib
 class VolunteerPortalServices:
 
     def hash_pass(password):
-        h = hashlib.new('ripemd160')
+        h = hashlib.new('SHA256')
         h.update(password.encode('utf-8'))
         return h.hexdigest()
